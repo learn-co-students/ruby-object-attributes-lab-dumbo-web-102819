a800624dda1bebf96f05b7f77f55ef0d2c698c0f
Frank Gerold <FrankETSGerold@gmail.com>
2019-10-29 12:05:12 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize#(person_name)
+    #@name = person_name
+  end
+  def name
+    @name
+  end
+
+  def name= (new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job= (job_name)
+    @job = job_name
+  end
+
+end
